6b53a0445431fdb1f2149bbe58b117824325ddbf
qzf <qiaozifeng@outlook.com>
2019-05-09 23:57:27 +0800

    include .h

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "select.cpp"
+#include "extmem.h"
+#include "util.h"
 
 int main(int argc, char const *argv[])
 {
     Buffer buf;
-    initBuffer(bufSize, blkSize, &buf);
+    initBuffer(BUFSIZE, BLKSIZE, &buf);
     generateRS(&buf);
     writeRS(R_BEGIN_DISK, S_BEGIN_DISK, &buf);
-    // linearSelect(40, 60, &buf);
-    // printBlock(0, &buf);
-    // printBlock(1, &buf);
+    linearSelect(40, 60, &buf);
+    printBlock(0, &buf);
+    printBlock(1, &buf);
 
-    mergeSortAllDisk(&buf);
-    writeRS(R_BEGIN_DISK, S_BEGIN_DISK, &buf);
+    // mergeSortAllDisk(&buf);
+    // writeRS(R_BEGIN_DISK, S_BEGIN_DISK, &buf);
     // binarySearch(40, 60, &buf);
     // printBlock(10, &buf);
     // printBlock(11, &buf);
+    // constructMap(&buf);
     return 0;
 }
--- util.cpp
+++ util.cpp
@@ -1,18 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "util.c"
+#include <string.h>
+#include <map>
+#include "util.h"
 
+using namespace std;
 
+map<unsigned int, int> RleftBound;
+map<unsigned int, int> RrightBound;
+map<unsigned int, int> SleftBound;
+map<unsigned int, int> SrightBound;
 
-void linearSelect(int R_A, int S_C, Buffer *buf);
-void mergeSortAllDisk(Buffer *buf);
-void mergeSort(int begin1, int begin2, int diskNum, int output, Buffer *buf);
-void swap(unsigned int *p1, unsigned int *p2);
-void sortDisk(int disk, Buffer *buf);
-void binarySearch(int R_A, int S_C, Buffer *buf);
-int* rightSearchEqual(int val, int disk, int *outInfo, Buffer *buf); 
-int* leftSearchEqual(int val, int disk, int *outInfo, Buffer *buf); 
-int* rightSearchNequal(int val, int disk, int* outInfo, Buffer *buf);
+void generateRS(Buffer *buf) {
+    int i, j;
+    int diskID = R_BEGIN_DISK;
+    int nextDisk, lastDisk;
+    unsigned char *blk_char = NULL;
+    unsigned int *blk_int = NULL;
+    for (i = 0; i < R_BLOCK_NUM; i++) {
+        blk_char = getNewBlockInBuffer(buf);
+        blk_int = (unsigned int*)blk_char;
+        for (j = 0; j < BLOCK_TUPLE_NUM; j++) {
+            *(blk_int+2*j) = rand()%(40)+1;
+            *(blk_int+2*j+1) = rand()%1000+1;
+        }
+        nextDisk = diskID+1;
+        lastDisk = diskID-1;
+        if (i == R_BLOCK_NUM-1) nextDisk = 0;
+        if (i == 0)  lastDisk = 0;
+        *(blk_int+2*j) = nextDisk;
+        *(blk_int+2*j+1) = lastDisk;
+        writeBlockToDisk(blk_char, diskID, buf);
+        freeBlockInBuffer(blk_char, buf);
+        diskID++;
+        nextDisk++;
+    }
+    diskID = S_BEGIN_DISK;
+    for (i = 0; i < S_BLOCK_NUM; i++) {
+        blk_char = getNewBlockInBuffer(buf);
+        blk_int = (unsigned int*)blk_char;
+        for (j = 0; j < BLOCK_TUPLE_NUM; j++) {
+            *(blk_int+2*j) = rand()%(41)+20;
+            *(blk_int+2*j+1) = rand()%1000+1;
+        }
+        nextDisk = diskID+1;
+        lastDisk = diskID-1;
+        if (i == S_BLOCK_NUM-1) nextDisk = 0;
+        if (i == 0)  lastDisk = 0;
+        *(blk_int+2*j) = nextDisk;
+        *(blk_int+2*j+1) = lastDisk;
+        writeBlockToDisk(blk_char, diskID, buf);
+        freeBlockInBuffer(blk_char, buf);
+        diskID++;
+        nextDisk++;
+    }
+}
+
+void printBlock(int disk, Buffer *buf) {
+    int i = 0;
+    unsigned char *blk_char;
+    unsigned int *blk_int;
+    blk_char = readBlockFromDisk(disk, buf);
+    blk_int = (unsigned int *)blk_char;
+    while (i < BLOCK_TUPLE_NUM) {
+        printf("%d\t%d\n", blk_int[2*i], blk_int[2*i+1]);
+        i++;
+    }
+    printf("next: %d\t last:%d\n", blk_int[2*i], blk_int[2*i+1]);
+    freeBlockInBuffer(blk_char, buf);
+}
+
+void writeRS(int R_beign, int S_begin, Buffer *buf) {
+    FILE *fp;
+    unsigned char *blk_char;
+    unsigned int *blk_int;
+    int i, j;
+    fp = fopen("R.txt", "w");
+    for (i = 0; i < R_BLOCK_NUM; i++) {
+        blk_char = readBlockFromDisk(R_beign+i, buf);
+        blk_int = (unsigned int *)blk_char;
+        fprintf(fp, "%d\n", R_beign+i);
+        for (j = 0; j < BLOCK_TUPLE_NUM; j++) {
+            fprintf(fp, "%d\t%d\n", blk_int[2*j], blk_int[2*j+1]);
+        }
+        fprintf(fp, "next: %d\t last: %d\n\n", blk_int[2*j], blk_int[2*j+1]);
+        freeBlockInBuffer(blk_char, buf);
+    }
+    fclose(fp);
+    
+    fp = fopen("S.txt", "w");
+    for (i = 0; i < S_BLOCK_NUM; i++) {
+        blk_char = readBlockFromDisk(S_begin+i, buf);
+        blk_int = (unsigned int *)blk_char;
+        fprintf(fp, "%d\n", S_begin+i);
+        for (j = 0; j < BLOCK_TUPLE_NUM; j++) {
+            fprintf(fp, "%d\t%d\n", blk_int[2*j], blk_int[2*j+1]);
+        }
+        fprintf(fp, "next: %d\t last: %d\n\n", blk_int[2*j], blk_int[2*j+1]);
+        freeBlockInBuffer(blk_char, buf);
+    }
+    fclose(fp);
+}
 
 void linearSelect(int R_A, int S_C, Buffer *buf) {
     int i, j;
@@ -525,4 +613,36 @@ int* rightSearchNequal(int val, int disk, int* outInfo, Buffer *buf) {
     freeBlockInBuffer(result_blk_char, buf);
     freeBlockInBuffer(blk_char, buf);
     return outInfo;
+}
+
+void constructMap(Buffer *buf) {
+    int i, j;
+    int v;
+    unsigned char *blk_char;
+    unsigned int *blk_int;
+    for (i = 0; i < R_BLOCK_NUM; i++) {
+        blk_char = readBlockFromDisk(R_BEGIN_DISK+i, buf);
+        blk_int = (unsigned int *)blk_char;
+        for (j = 0; j < BLOCK_TUPLE_NUM; j++) {
+            v = blk_int[2*j];
+            if (RleftBound[v] == 0) RleftBound[v] = R_BEGIN_DISK+i;
+            if (R_BEGIN_DISK+i > RrightBound[v])    RrightBound[v] = R_BEGIN_DISK+i;
+        }
+        freeBlockInBuffer(blk_char, buf);
+    }
+    for (i = 0; i < S_BLOCK_NUM; i++) {
+        blk_char = readBlockFromDisk(S_BEGIN_DISK+i, buf);
+        blk_int = (unsigned int *)blk_char;
+        for (j = 0; j < BLOCK_TUPLE_NUM; j++) {
+            v = blk_int[2*j];
+            if (SleftBound[v] == 0) SleftBound[v] = S_BEGIN_DISK+i;
+            if (S_BEGIN_DISK+i > SrightBound[v])    SrightBound[v] = S_BEGIN_DISK+i;
+        }
+        freeBlockInBuffer(blk_char, buf);
+    }
+    
+    // for (iter = RrightBound.begin(); iter != RrightBound.end(); iter++) {
+    //     map<unsigned int, int>::iterator iter;
+    //     cout << iter->first << "  " << iter->second << endl;
+    // }
 }
